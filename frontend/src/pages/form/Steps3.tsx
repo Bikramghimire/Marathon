541f263235a3caf89649415c2a29a0d273f85a13
bikram ghimire <ikramghimire@gmail.com>
2022-12-24 01:04:09 +0545

    updates on step 3 of formik

--- frontend/src/pages/form/Steps3.tsx
+++ frontend/src/pages/form/Steps3.tsx
@@ -1,4 +1,5 @@
 import { Form } from "antd";
+import ErrorList from "antd/es/form/ErrorList";
 import { Field, Formik } from "formik";
 import React from "react";
 
@@ -9,26 +10,51 @@ const Steps3 = () => {
       <Formik
         initialValues={{ email: "", firstName: "", lastName: "" }}
         onSubmit={(values) => {
+          console.log("fdsakjfkjsaf");
           setTimeout(() => {
             alert(JSON.stringify(values));
           }, 400);
         }}
+        validate={(values: any) => {
+          const errors = { firstName: "", lastName: "", email: "" };
+          if (!values.firstName) {
+            errors.firstName = "required";
+          } else if (values.firstName.length > 15) {
+            errors.firstName = "must be 15 characters or less";
+          }
+          if (!values.lastName) {
+            errors.lastName = "required";
+          } else if (values.lastName.length > 15) {
+            errors.lastName = "must be 15 charaters or less";
+          }
+          if (!values.email) {
+            errors.email = "required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+          ) {
+            errors.email = "invalid email address";
+          }
+          return errors;
+        }}
       >
-        {({ handleChange, handleSubmit, values }) => {
+        {({ handleChange, handleSubmit, values, errors }) => {
           console.log("the values area======", values);
           return (
             <form onSubmit={handleSubmit}>
               <div>
                 <label htmlFor="email">email:</label>
                 <Field name="email" type="email" />
+                {errors.email && <p>{errors.email}</p>}
               </div>
               <div>
-                <label htmlFor="firstname">firstName:</label>
+                <label htmlFor="firstName">firstName:</label>
                 <Field name="firstName" type="text" />
+                {errors.firstName && <p>{errors.firstName}</p>}
               </div>
               <div>
                 <label htmlFor="lastname">lastName:</label>
                 <Field name="lastName" type="text" />
+                {errors.firstName && <p>{errors.lastName}</p>}
               </div>
               <button type="submit">submitting</button>
             </form>
