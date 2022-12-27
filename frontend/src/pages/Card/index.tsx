745f069c62272dea2d5384c8938eaf1903082b88
bikram ghimire <ikramghimire@gmail.com>
2022-12-27 22:20:28 +0545

    dmajfsafsadf

--- frontend/src/pages/Card/index.tsx
+++ frontend/src/pages/Card/index.tsx
@@ -4,7 +4,7 @@ import perfume from "../../assets/perfume.jpg";
 const Container = styled.div`
   width: 100%;
   max-width: 600px;
-  height: 500px;
+  height: 400px;
   margin: 30px auto;
   border-radius: 16px;
   border: 1px solid #ddd;
@@ -31,7 +31,36 @@ const Title = styled.h2`
   line-height: 1.5rem;
   padding: 26px 0px;
 `;
+const Price = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  padding: 20px;
+`;
+const Cost = styled.h3`
+  color: hsl(158, 36%, 37%);
+  font-size: 30px;
+`;
+const ActualCost = styled.div`
+  text-decoration-line: line-through;
+`;
 
+const AddToCart = styled.button`
+  padding: 16px 30px;
+  outlined: none;
+  font-size: 16px;
+  font-weight: 700;
+  color: white;
+  background-color: hsl(158, 36%, 37%);
+  border: none;
+  border-radius: 8px;
+  width: 260px;
+  justify-content: center;
+  &:hover {
+    background-color: hsl(228, 12%, 48%);
+  }
+`;
 const Paragraph = styled.p``;
 const Card = () => {
   return (
@@ -46,6 +75,11 @@ const Card = () => {
             application in the name of the lord. change the science and physic
             and biology
           </Paragraph>
+          <Price>
+            <Cost>$149.99</Cost>
+            <ActualCost>169.99</ActualCost>
+          </Price>
+          <AddToCart>Add To Cart</AddToCart>
         </ContentWrapper>
       </Wrapper>
     </Container>
