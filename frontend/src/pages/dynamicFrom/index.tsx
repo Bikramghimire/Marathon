93f5deeb53feb274f87c5cb049ec58348224dae0
bikram ghimire <ikramghimire@gmail.com>
2022-12-31 01:27:10 +0545

    added the custom hook for fetching data using hook

--- frontend/src/pages/dynamicFrom/index.tsx
+++ frontend/src/pages/dynamicFrom/index.tsx
@@ -1,10 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Modal } from "antd";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DatePicker from "./components/fields/DatePicker";
 import InputField from "./components/fields/InputField";
 import AddEditForm from "./containers/AddForm";
 
 const Dynamic = () => {
+  const [rentalHistoryData, setRentalHistoryData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/rentalHistory")
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setRentalHistoryData(data);
+      })
+      .catch((err) => console.log("the error===", err));
+  }, []);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const showModal = () => {
@@ -31,6 +42,33 @@ const Dynamic = () => {
 
       <h3>Rental History</h3>
       <button onClick={showModal}>Add RentalHistory</button>
+      {rentalHistoryData?.map((item: any) => {
+        return (
+          <div
+            style={{
+              width: "400px",
+              height: "100px",
+              padding: "20px",
+              border: "1px solid red",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "start",
+              justifyContent: "space-around",
+            }}
+          >
+            <div>
+              <h3>{item.landlordName}</h3>
+              <h3>{item.landlordEmail}</h3>
+              <p>{item.propertyAddress}</p>
+              <p>
+                {item.startingDate}-{item.endingDate}
+              </p>
+            </div>
+            <button>edit</button>
+            <button>delete</button>
+          </div>
+        );
+      })}
     </div>
   );
 };
