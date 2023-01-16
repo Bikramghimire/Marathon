f1e24846b94dac5d988f511442043c0d103d2868
bikram ghimire <ikramghimire@gmail.com>
2023-01-16 22:14:36 +0545

    remain for active

--- frontend/src/pages/moment/index.tsx
+++ frontend/src/pages/moment/index.tsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
+import styled from "styled-components";
+const Container = styled.div`
+  border: 1px solid red;
+  display: flex;
+  flex-direction: row;
+`;
+const Calender = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100px;
+  border: 1px solid green;
+  .active {
+    background-color: red;
+  }
+`;
 const Moment = () => {
   console.log("the momet=====", moment());
   const getWeeks = () => {
-    return moment().startOf("week");
+    const weekFirstDate = moment().add(1, "days");
+    return new Array(7).fill(0).map((item, index) => {
+      return weekFirstDate.clone().add(index, "days");
+    });
   };
-  const week = moment().startOf("week").format("MM");
-  const weekFirstDate = moment().add(1, "days");
-
-  console.log(
-    "array===",
-    new Array(7).fill(0).map((item, index) => {
-      return weekFirstDate.add(index, "days").format("DD");
-    })
-  );
+  const todayDay = () => {
+    return moment();
+  };
+  const [sevenDaysWeek, setSevenDaysWeek] = useState(getWeeks());
+  const [todaysDay, setTodaysDay] = useState(todayDay());
+  const weeks = ["sun", "mon", "tue", "wed", "thus", "fri", "sat"];
+  console.log("the seven", sevenDaysWeek, todayDay());
 
   return (
-    <div>
-      <>this is the moment</>
-    </div>
+    <Container>
+      {sevenDaysWeek.map((item, index) => {
+        return (
+          <Calender key={index}>
+            {item === todaysDay ? <div>{weeks[index]}</div> : ""}
+            <div>{item.format("DD")}</div>
+          </Calender>
+        );
+      })}
+    </Container>
   );
 };
 
